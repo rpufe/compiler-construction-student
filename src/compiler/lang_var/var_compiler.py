4a30fad9d7b7f3063293f7d94212d622fdad19ca
Richard Pufe <rpufe@stud.hs-offenburg.de>
2024-03-21 16:09:26 +0100

    created file

---
+++ src/compiler/lang_var/var_compiler.py
@@ -0,0 +1,8 @@
+from lang_var.var_ast import *
+from common.wasm import *
+import lang_var.var_tychecker as var_tychecker
+from common.compilerSupport import *
+import common.utils as utils
+
+def compileModule(m: mod, cfg: CompilerConfig) -> WasmModule:
+    pass
